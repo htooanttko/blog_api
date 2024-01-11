adb75fcf5109cd2ed95968180b9357abe318a849
htooanttko <kohtooantt@gmail.com>
2024-01-11 19:06:44 +0630

    third changes

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,3 +1,7 @@
-const allowedOrigin = ["www.your-web-domain.com", "http://localhost:3500"];
+const allowedOrigin = [
+  "www.your-web-domain.com",
+  "http://localhost:3500",
+  "https://node-blog-dun.vercel.app/",
+];
 
 module.exports = allowedOrigin;
